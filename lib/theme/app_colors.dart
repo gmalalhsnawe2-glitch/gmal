6adaba59f739831ca3082142360bfd67870b7d16
gmalalhsnawe2-glitch <gmalalhsnawe2@gmail.com>
2026-09-02 02:07:24 +0300

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0F0F13);
+  static const Color cardBg = Color(0xFF16161E);
+  static const Color primaryRed = Colors.redAccent;
+  static const Color accentGold = Colors.amber;
+  static const Color textWhite = Colors.white;
+  static const Color textGrey = Colors.grey;
+}
