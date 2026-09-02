0206deb628e6575e51818a9d3fa5ffbc6dc327a1
gmalalhsnawe2-glitch <gmalalhsnawe2@gmail.com>
2026-09-02 14:46:35 +0300

    Update app_colors.dart

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background = Color(0xFF0F0F13);
-  static const Color cardBg = Color(0xFF16161E);
-  static const Color primaryRed = Colors.redAccent;
-  static const Color accentGold = Colors.amber;
+  static const Color backgroundColor = Color(0xFF121212);
+  static const Color cardBg = Color(0xFF1E1E1E);
+  static const Color primaryGold = Color(0xFFFFD700);
   static const Color textWhite = Colors.white;
   static const Color textGrey = Colors.grey;
 }
